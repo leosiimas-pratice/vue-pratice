916dc993cbe35107e1f34edd80516f47aa5dae0e
Leonardo Abreu <abreu.leo@hotmail.com>
2019-07-31 01:59:15 -0300

    animacao e transicao parte 2

--- animacoes-exercicios/src/App.vue
+++ animacoes-exercicios/src/App.vue
@@ -25,10 +25,24 @@
 			<option value="slide">Slide</option>
 		</b-select>
 
-		<transition :name="tipoAnimacao" mode="out-in">
+		<!-- <transition :name="tipoAnimacao" mode="out-in">
 			<b-alert variant="info" show v-if="exibir" key="info">{{ msg }}</b-alert>
 			<b-alert variant="warning" show v-else key="warning">{{ msg }}</b-alert>
-		</transition>		
+		</transition> -->
+
+		<hr>
+		<button @click="exibir2 = !exibir2">Alternar</button>
+		<transition
+			@before-enter="beforeEnter"
+			@enter="enter"
+			@after-enter="afterEnter"
+			@enter-cancelled="enterCancelled"
+			@before-leave="beforeLeave"
+			@leave="leave"
+			@after-leave="afterLeave"
+			@leave-cancelled="leaveCancelled">
+			<div v-if="exibir2" class="caixa"></div>
+		</transition>
 	</div>
 </template>
 
@@ -39,8 +53,51 @@ export default {
 		return {
 			msg: 'Uma mensagem de informação para o usuário!',
 			exibir: false,
-			tipoAnimacao: 'fade'
+			exibir2: false,
+			tipoAnimacao: 'fade',
+			larguraBase: 0,
 		}
+	},
+	methods:{
+		animar(el,done,negativo){
+			let rodada = 1
+			const temporizador = setInterval(() => {
+				const novaLargura = this.larguraBase + 
+					(negativo ? -rodada * 10 : rodada * 10)
+				el.style.width = `${novaLargura}px`
+				rodada++
+				if(rodada > 30){
+					clearInterval(temporizador)
+					done()
+				}
+			}, 20)
+		},
+		beforeEnter (el){
+			this.larguraBase = 0
+			el.style.width  = `${this.larguraBase}px`
+		},
+		enter(el, done) {
+			this.animar(el,done,false)
+		},
+		/* afterEnter(el) {
+			console.log('afterEnter')
+		},
+		enterCancelled() {
+			console.log('enterCancelled')
+		}, */
+		beforeLeave (el) {
+			this.larguraBase = 300
+			el.style.width  = `${this.larguraBase}px`
+		},
+		leave(el, done) {
+			this.animar(el,done,true)
+		},
+		/* afterLeave(el) {
+			console.log('afterLeave')
+		},
+		leaveCancelled() {
+			console.log('LeaveCancelled')
+		} */
 	}
 }
 </script>
@@ -54,6 +111,13 @@ export default {
 	color: #2c3e50;
 	margin-top: 60px;
 	font-size: 1.5rem;
+}
+
+.caixa{
+	height: 100px;
+	width: 300px;
+	margin: 30px auto;
+	background-color: lightgreen;
 }
 
 .fade-enter, .fade-leave-to{
